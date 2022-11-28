70ddf18bd3bda5a5d971900702dd4e5d04dbbc90
Chris Ward <tjcw@uk.ibm.com>
2022-11-28 09:15:46 +0000

    Run script for client/server TCP performance testing
    Signed-off-by: Chris Ward <tjcw@uk.ibm.com>

---
+++ AF_XDP-filter/iperf3-real/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+# SPDX-License-Identifier: GPL-2.0
+# Run script for eBPF TCP performance testing between 2 real machines
+# Run this on the server. Assumes server IP address is 10.1.0.2
+# Assumes client is reachable on another network as 192.168.17.9
+ip link set dev enp25s0 xdpgeneric off
+rm -f /sys/fs/bpf/accept_map /sys/fs/bpf/xdp_stats_map
+ip tuntap add mode tun tun0
+ip link set dev tun0 down
+ip link set dev tun0 addr 10.1.0.254/24
+ip link set dev tun0 up
+for device in /proc/sys/net/ipv4/conf/*
+do
+  echo 0 >${device}/rp_filter
+done
+export LD_LIBRARY_PATH=/usr/local/lib
+./af_xdp_user -S -d enp25s0 -Q 16 --filename ./af_xdp_kern.o &
+real_pid=$!
+iperf3 -s &
+iperf3_pid=$!
+ssh 192.168.17.9 iperf3 -c 10.1.0.2 | tee client.log
+kill -INT ${iperf3_pid} ${real_pid}
+wait
+
