46c010f067e16265085ec4569d8d8ce6238c815f
Chris Ward <tjcw@uk.ibm.com>
2022-11-28 15:39:45 +0000

    Disable Nagle
    Signed-off-by: Chris Ward <tjcw@uk.ibm.com>

--- AF_XDP-filter/netperf-namespace/runns1.sh
+++ AF_XDP-filter/netperf-namespace/runns1.sh
@@ -6,5 +6,5 @@ ip link set lo up
 ip link set vpeer1 up
 ip addr add 10.10.0.10/16 dev vpeer1
 sleep 6
-netperf -4 -t TCP_RR -H 10.10.0.20 -p 50000
-netperf -4 -t TCP_CRR -H 10.10.0.20 -p 50000
+netperf -4 -t TCP_RR -H 10.10.0.20 -p 50000 -- -D
+netperf -4 -t TCP_CRR -H 10.10.0.20 -p 50000 -- -D
