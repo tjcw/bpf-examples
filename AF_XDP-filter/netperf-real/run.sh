fdb8fd1b5570cbea32f4f93f8c71be6e407913b1
Chris Ward <tjcw@uk.ibm.com>
2022-11-30 12:08:26 +0000

    explore why not getting anything into the log files
    Signed-off-by: Chris Ward <tjcw@uk.ibm.com>

--- AF_XDP-filter/netperf-real/run.sh
+++ AF_XDP-filter/netperf-real/run.sh
@@ -9,9 +9,9 @@
 
 function sequence_tests
 {
-  ssh -n 192.168.17.9 netperf -4 -t TCP_RR -H 10.1.0.2 -p 50000 -- -D | tee client_rr.log
-  ssh -n 192.168.17.9 netperf -4 -t TCP_CRR -H 10.1.0.2 -p 50000 -- -D | tee client_crr.log
-  ssh -n 192.168.17.9 netperf -4 -t TCP_STREAM -H 10.1.0.2 -p 50000 -- -D | tee client_stream.log
+  ssh -n 192.168.17.9 netperf -4 -t TCP_RR -H 10.1.0.2 -p 50000 -- -D 2>&1 | tee client_rr.log
+  ssh -n 192.168.17.9 netperf -4 -t TCP_CRR -H 10.1.0.2 -p 50000 -- -D 2>&1 | tee client_crr.log
+  ssh -n 192.168.17.9 netperf -4 -t TCP_STREAM -H 10.1.0.2 -p 50000 -- -D 2>&1 | tee client_stream.log
 }
 
 ip link set dev enp25s0 xdpgeneric off
