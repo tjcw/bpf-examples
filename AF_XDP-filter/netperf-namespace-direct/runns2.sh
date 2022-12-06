41abdbe2ba1435d220836e3db82f792e6b059342
Chris Ward <tjcw@uk.ibm.com>
2022-12-06 15:55:48 +0000

    scripts to run namespace test with filter starting on root

---
+++ AF_XDP-filter/netperf-namespace-direct/runns2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+# SPDX-License-Identifier: GPL-2.0
+# Server side helper script for TCP performance testing with eBPF filter
+# Set FILTER env var to af_xdp_kern or af_xdp_kern_passall according to which filter to use
+# Set LEAVE env var non-null for baseline test with no eBPF filter
+# Set TCPDUMP env var non-null to take tcpdumps of the interfaces
+ip link set lo up
+ip link set vpeer2 up
+ip addr add 10.10.0.20/16 dev vpeer2
+ip link set dev vpeer2 xdpgeneric off
+if [[ -n "${TCPDUMP}" ]]
+then
+  tcpdump -i tun0 -w tun0.tcpdump &
+  tcpdump_tun0_pid=$!
+  tcpdump -i vpeer2 -w vpeer2.tcpdump &
+  tcpdump_vpeer2_pid=$!
+fi
+
+netserver -p ${PORT} -4 -D -f &
+netserver_pid=$!
+sleep 120
+kill -INT ${netserver_pid}  
+wait
+if [[ -n "${TCPDUMP}" ]]
+then
+  kill -INT ${tcpdump_tun0_pid}
+  kill -INT ${tcpdump_vpeer2_pid}
+fi
+wait
