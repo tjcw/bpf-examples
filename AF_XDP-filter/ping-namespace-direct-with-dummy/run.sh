54e687c0918099fef3944fdfc7b34ccb4faa07b4
Chris Ward <tjcw@uk.ibm.com>
2022-12-09 10:38:12 +0000

    interpret -a 1 as dummy. Set -a 1 in script

--- AF_XDP-filter/ping-namespace-direct-with-dummy/run.sh
+++ AF_XDP-filter/ping-namespace-direct-with-dummy/run.sh
@@ -61,7 +61,7 @@ fi
     done
     destination_mac=$(ip a s dev veth2|awk '{ if($1 == "link/ether") { print $2 } }')
     source_mac=$(ip a s dev veth2|awk '{ if($1 == "link/ether") { print $2 } }')
-    DST_MAC=${destination_mac} SRC_MAC=${source_mac} ../af_xdp_user -S -d veth1 -Q 1 --filename ../${FILTER}.o -r veth2 &
+    DST_MAC=${destination_mac} SRC_MAC=${source_mac} ../af_xdp_user -S -d veth1 -Q 1 --filename ../${FILTER}.o -r veth2 -a 1 &
     af_pid=$!
     sleep 2
     ../af_xdp_user_dummy -S -d veth2 -Q 1 --filename ../af_xdp_kern_dummy.o &
