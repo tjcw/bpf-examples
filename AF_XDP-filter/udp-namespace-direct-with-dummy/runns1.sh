47cc5fccf3099e7dbffe877101fc58b77acc3821
Chris Ward <tjcw@uk.ibm.com>
2022-12-12 11:10:37 +0000

    Baseline for udp test

---
+++ AF_XDP-filter/udp-namespace-direct-with-dummy/runns1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+# SPDX-License-Identifier: GPL-2.0
+# Client side helper script to run ping forfinctional testing
+# in a namespace with the eBPF filter
+ip link set lo up
+ip link set vpeer1 up
+ip addr add 10.10.0.10/16 dev vpeer1
+if [[ -n "${TCPDUMP}" ]]
+then
+  tcpdump -i vpeer1 -w vpeer1.tcpdump not ip6 &
+  tcpdump_vpeer1_pid=$!
+fi
+sleep 6
+ping -c 2 10.10.0.20
+if [[ -n "${TCPDUMP}" ]]
+then
+  kill -INT ${tcpdump_vpeer1_pid}
+fi
+wait
