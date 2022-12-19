a44b11c8fefa5cf6a1a9dcc209374ccb6dde680d
Chris Ward <tjcw@uk.ibm.com>
2022-12-19 14:47:17 +0000

    run script to drive various tests

--- AF_XDP-filter/run-all.sh
+++ AF_XDP-filter/run-all.sh
@@ -5,8 +5,6 @@
   FILTER=af_xdp_kern TCP_RR=1 PORT=50000 ./run.sh
   FILTER=af_xdp_kern_passall TCP_RR=1 PORT=50001 ./run.sh
   LEAVE=1 TCP_RR=1 PORT=50002 ./run.sh
-  sleep 260
-  netstat -p -t
 )
 (
   cd netperf-real
@@ -14,8 +12,6 @@
   FILTER=af_xdp_kern TCP_RR=1 PORT=50003 ./run.sh
   FILTER=af_xdp_kern_passall TCP_RR=1 PORT=50004 ./run.sh
   LEAVE=1 TCP_RR=1 PORT=50005 ./run.sh
-  sleep 260
-  netstat -p -t
 )
 (
   cd iperf3-namespace
@@ -32,4 +28,27 @@
   FILTER=af_xdp_kern_passall TCP_RR=1 PORT=50010 ./run.sh
   LEAVE=1 TCP_RR=1 PORT=50011 ./run.sh
 )
-
+(
+  cd ping-namespace
+  FILTER=af_xdp_kern ./run.sh
+)
+(
+  cd ping-namespace-direct
+  FILTER=af_xdp_kern ./run.sh
+)
+(
+  cd ping-namespace-direct-with-dummy
+  FILTER=af_xdp_kern ./run.sh
+)
+(
+  cd ping-namespace-direct-with-dummy-at-source
+  FILTER=af_xdp_kern ./run.sh
+)
+(
+  cd ping-namespace-direct-with-dummy-at-veth1
+  FILTER=af_xdp_kern ./run.sh
+)
+(
+  cd ping-namespace-direct-with-dummy-at-bridge
+  FILTER=af_xdp_kern ./run.sh
+)
