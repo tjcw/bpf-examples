e51d567e5b664ed5617b74ba14fe67de62e708fa
Chris Ward <tjcw@uk.ibm.com>
2022-12-19 15:02:13 +0000

    Knock out the tests which require the 100gE real network

--- AF_XDP-filter/run-all.sh
+++ AF_XDP-filter/run-all.sh
@@ -6,13 +6,13 @@
   FILTER=af_xdp_kern_passall TCP_RR=1 PORT=50001 ./run.sh
   LEAVE=1 TCP_RR=1 PORT=50002 ./run.sh
 )
-(
-  cd netperf-real
-  echo "netperf-real"
-  FILTER=af_xdp_kern TCP_RR=1 PORT=50003 ./run.sh
-  FILTER=af_xdp_kern_passall TCP_RR=1 PORT=50004 ./run.sh
-  LEAVE=1 TCP_RR=1 PORT=50005 ./run.sh
-)
+#(
+#  cd netperf-real
+#  echo "netperf-real"
+#  FILTER=af_xdp_kern TCP_RR=1 PORT=50003 ./run.sh
+#  FILTER=af_xdp_kern_passall TCP_RR=1 PORT=50004 ./run.sh
+#  LEAVE=1 TCP_RR=1 PORT=50005 ./run.sh
+#)
 (
   cd iperf3-namespace
   echo "iperf3-namespace"
@@ -20,14 +20,14 @@
   FILTER=af_xdp_kern_passall TCP_RR=1 PORT=50007 ./run.sh
   LEAVE=1 TCP_RR=1 PORT=50008 ./run.sh
 )
-(
-  cd iperf3-real
-  echo "iperf3-real"
-
-  FILTER=af_xdp_kern TCP_RR=1 PORT=50009  ./run.sh
-  FILTER=af_xdp_kern_passall TCP_RR=1 PORT=50010 ./run.sh
-  LEAVE=1 TCP_RR=1 PORT=50011 ./run.sh
-)
+#(
+#  cd iperf3-real
+#  echo "iperf3-real"
+#
+#  FILTER=af_xdp_kern TCP_RR=1 PORT=50009  ./run.sh
+#  FILTER=af_xdp_kern_passall TCP_RR=1 PORT=50010 ./run.sh
+#  LEAVE=1 TCP_RR=1 PORT=50011 ./run.sh
+#)
 (
   cd udp-namespace-direct-with-dummy
   ./teardown.sh
