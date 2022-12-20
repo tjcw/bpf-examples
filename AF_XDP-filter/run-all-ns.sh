25c600069cf1fc3fdc98b9c75e6c605ccdd11164
Chris Ward <tjcw@uk.ibm.com>
2022-12-20 13:12:18 +0000

    netperf-namespace-direct into run-all-ns.sh

--- AF_XDP-filter/run-all-ns.sh
+++ AF_XDP-filter/run-all-ns.sh
@@ -7,6 +7,13 @@
   LEAVE=1 TCP_RR=1 TCP_STREAM=1 TCP_CRR=1 PORT=50002 ./run.sh
 )
 (
+  cd netperf-namespace-direct
+  echo "netperf-namespace-direct"
+  FILTER=af_xdp_kern TCP_RR=1 TCP_STREAM=1 TCP_CRR=1 PORT=50000 ./run.sh
+  FILTER=af_xdp_kern_passall TCP_STREAM=1 TCP_CRR=1 TCP_RR=1 PORT=50001 ./run.sh
+  LEAVE=1 TCP_RR=1 TCP_STREAM=1 TCP_CRR=1 PORT=50002 ./run.sh
+)
+(
   cd iperf3-namespace
   echo "iperf3-namespace"
   FILTER=af_xdp_kern PORT=50006 ./run.sh
