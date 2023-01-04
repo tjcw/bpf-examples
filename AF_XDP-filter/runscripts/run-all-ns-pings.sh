aaa4c091dbb4b3e39d766ebca495469c1ac37cd5
Chris Ward <tjcw@uk.ibm.com>
2023-01-04 13:49:11 +0000

    script to run all namespace pings

---
+++ AF_XDP-filter/runscripts/run-all-ns-pings.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+ulimit -c unlimited
+(
+  cd ping-namespace
+  FILTER=af_xdp_kern ./run.sh
+)
+(
+  cd ping-namespace-direct
+  FILTER=af_xdp_kern ./run.sh
+)
+(
+  cd ping-namespace-direct-with-dummy
+  FILTER=af_xdp_kern ./run.sh
+)
+(
+  cd ping-namespace-direct-with-dummy-at-source
+  FILTER=af_xdp_kern ./run.sh
+)
+(
+  cd ping-namespace-direct-with-dummy-at-veth1
+  FILTER=af_xdp_kern ./run.sh
+)
+(
+  cd ping-namespace-direct-with-dummy-at-bridge
+  FILTER=af_xdp_kern ./run.sh
+)
