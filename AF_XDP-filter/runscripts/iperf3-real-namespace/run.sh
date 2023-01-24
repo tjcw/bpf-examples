13875c986a5244952b6f5f10a210893faacd0363
Chris Ward <tjcw@uk.ibm.com>
2023-01-24 09:42:03 +0000

    first pass at run script between real machines with a namespace

---
+++ AF_XDP-filter/runscripts/iperf3-real-namespace/run.sh
@@ -0,0 +1,67 @@
+#!/bin/bash -x
+# SPDX-License-Identifier: GPL-2.0
+# Run script for eBPF TCP performance testing between 2 real machines
+# Run this on the server. Assumes server IP address is ${SERVER_IP}
+# Assumes client is reachable on another network as ${CLIENT_IP}
+# Arranged for a Pensando NIC which uses 16 queues.
+# Set FILTER env var to af_xdp_kern or af_xdp_kern_passall according to which filter to use
+# Set LEAVE env var non-null for baseline test with no eBPF filter
+# Set PORT to choose a port for the server to listen on
+# Set TUN_IP, CLIENT_IP, and SERVER_IP as required
+
+ip link set dev enp25s0 xdpgeneric off
+rm -f /sys/fs/bpf/accept_map /sys/fs/bpf/xdp_stats_map
+#ip tuntap add mode tun tun0
+#ip link set dev tun0 down
+#ip addr add ${TUN_IP}/24 dev tun0
+#ip link set dev tun0 up
+ip link delete veth1
+ip link delete veth2
+
+ip netns delete ns1
+ip netns delete ns2
+
+rm -f vpeer1.tcpdump vpeer2.tcpdump veth1.tcpdump veth2.tcpdump
+
+ip netns add ns1
+ip link add veth1 type veth peer name vpeer1
+ip link set veth1 up
+ip link set vpeer1 netns ns1
+
+ip link add br0 type bridge
+ip link set br0 up
+ip addr add 10.2.0.1/16 dev br0
+
+ip link set veth1 master br0
+ip link set enp25s0 master br0
+
+ssh ${CLIENT_IP} route add host 10.2.0.2 gw 10.1.0.2
+if [[ -z "${LEAVE}" ]]
+then 
+  for device in /proc/sys/net/ipv4/conf/*
+  do
+    echo 2 >${device}/rp_filter
+  done
+  cd ../..
+  ip netns exec ns1 iperf3 -s -p ${PORT}  &
+  iperf3_pid=$!
+  sleep 2
+  ./af_xdp_user -S -d veth1 -Q 16 --filename ./${FILTER}.o -r vpeer1 -a ${iperf3_pid} &
+  real_pid=$!
+  sleep 2
+  ssh ${CLIENT_IP} iperf3 -c 10.2.0.2 -p ${PORT} | tee client.log
+  kill -INT ${iperf3_pid} ${real_pid}
+  for device in /proc/sys/net/ipv4/conf/*
+  do
+    echo 2 >${device}/rp_filter
+  done
+else
+  ip netns exec ns1 iperf3 -s -p ${PORT} &
+  iperf3_pid=$!
+  ssh ${CLIENT_IP} iperf3 -c 10.2.0.2 -p ${PORT} | tee client.log
+  kill -INT ${iperf3_pid}
+fi
+wait
+ssh ${CLIENT_IP} route del host 10.2.0.2 gw 10.1.0.2
+
+
