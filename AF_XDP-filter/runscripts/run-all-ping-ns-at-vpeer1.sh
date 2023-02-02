5d20cdd5e0e1abefe4f8d9df7ab047fdc4e41f80
Chris Ward <tjcw@uk.ibm.com>
2023-02-02 15:20:42 +0000

    revise run-all-ping-ns-at-vpeer1

--- AF_XDP-filter/runscripts/run-all-ping-ns-at-vpeer1.sh
+++ AF_XDP-filter/runscripts/run-all-ping-ns-at-vpeer1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 ulimit -c unlimited
 (
-  cd iperf3-namespace-at-vpeer1
-  echo "iperf3-namespace-at-vpeer1"
-#  LEAVE=1 RR=1 PORT=50008 ./run.sh
-##  FILTER=af_xdp_kern_passall PORT=50007 ./run.sh
-#  FILTER=af_xdp_kern_dummy PORT=50009 DUMMY=1 ./run.sh
-#  FILTER=af_xdp_kern PORT=50006 ./run.sh
+  cd ping-namespace-at-vpeer1
+  echo "ping-namespace-at-vpeer1"
+  LEAVE=1 RR=1 PORT=50008 ./run.sh
+#  FILTER=af_xdp_kern_passall PORT=50007 ./run.sh
+  FILTER=af_xdp_kern_dummy PORT=50009 DUMMY=1 ./run.sh
+  FILTER=af_xdp_kern PORT=50006 ./run.sh
   FILTER=af_xdp_kern PORT=50007 BOTH=1 ./run.sh
 )
