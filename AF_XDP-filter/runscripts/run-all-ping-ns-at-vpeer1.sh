17eee5ded3d61e6bebaa8c9cb1a71775776364c4
Chris Ward <tjcw@uk.ibm.com>
2023-02-03 12:22:13 +0000

    See if ping works on the all-packets intercept

--- AF_XDP-filter/runscripts/run-all-ping-ns-at-vpeer1.sh
+++ AF_XDP-filter/runscripts/run-all-ping-ns-at-vpeer1.sh
@@ -3,9 +3,10 @@ ulimit -c unlimited
 (
   cd ping-namespace-at-vpeer1
   echo "ping-namespace-at-vpeer1"
-  LEAVE=1 RR=1 PORT=50008 ./run.sh
-#  FILTER=af_xdp_kern_passall PORT=50007 ./run.sh
-  FILTER=af_xdp_kern_dummy PORT=50009 DUMMY=1 ./run.sh
-  FILTER=af_xdp_kern PORT=50006 ./run.sh
-  FILTER=af_xdp_kern PORT=50007 BOTH=1 ./run.sh
+#  LEAVE=1 RR=1 PORT=50008 ./run.sh
+##  FILTER=af_xdp_kern_passall PORT=50007 ./run.sh
+#  FILTER=af_xdp_kern_dummy PORT=50009 DUMMY=1 ./run.sh
+#  FILTER=af_xdp_kern PORT=50006 ./run.sh
+  FILTER=af_xdp_kern PORT=50008 ALL_PACKETS=1 ./run.sh
+#  FILTER=af_xdp_kern PORT=50007 BOTH=1 ./run.sh
 )
