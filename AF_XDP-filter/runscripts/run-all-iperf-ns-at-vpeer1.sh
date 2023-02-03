229bf8a9ff78c08fce33b3a9708e86bc8ed0f25f
Chris Ward <tjcw@uk.ibm.com>
2023-02-03 16:21:02 +0000

    Back to running test which are viable

--- AF_XDP-filter/runscripts/run-all-iperf-ns-at-vpeer1.sh
+++ AF_XDP-filter/runscripts/run-all-iperf-ns-at-vpeer1.sh
@@ -3,12 +3,12 @@ ulimit -c unlimited
 (
   cd iperf3-namespace-at-vpeer1
   echo "iperf3-namespace-at-vpeer1"
-#  LEAVE=1 RR=1 PORT=50008 ./run.sh
-#  FILTER=af_xdp_kern_passall PORT=50009 ./run.sh
-#  FILTER=af_xdp_kern_dummy PORT=50010 DUMMY=1 ./run.sh
-#  SWCKSUM=1 FILTER=af_xdp_kern PORT=50011 ./run.sh
-#  SWCKSUM=1 FILTER=af_xdp_kern PORT=50012 BOTH=1 ./run.sh
-#  SWCKSUM=1 FILTER=af_xdp_kern PORT=50013 ALL_PACKETS=1 ./run.sh
-#  SWCKSUM=1 FILTER=af_xdp_kern PORT=50014 BOTH=1 ALL_PACKETS=1 ./run.sh
-  INGRESS=1 SWCKSUM=1 FILTER=af_xdp_kern PORT=50014 BOTH=1 ALL_PACKETS=1 ./run.sh  
+  LEAVE=1 RR=1 PORT=50008 ./run.sh
+  FILTER=af_xdp_kern_passall PORT=50009 ./run.sh
+  FILTER=af_xdp_kern_dummy PORT=50010 DUMMY=1 ./run.sh
+  SWCKSUM=1 FILTER=af_xdp_kern PORT=50011 ./run.sh
+  SWCKSUM=1 FILTER=af_xdp_kern PORT=50012 BOTH=1 ./run.sh
+  SWCKSUM=1 FILTER=af_xdp_kern PORT=50013 ALL_PACKETS=1 ./run.sh
+  SWCKSUM=1 FILTER=af_xdp_kern PORT=50014 BOTH=1 ALL_PACKETS=1 ./run.sh
+#  INGRESS=1 SWCKSUM=1 FILTER=af_xdp_kern PORT=50014 BOTH=1 ALL_PACKETS=1 ./run.sh  
 )
